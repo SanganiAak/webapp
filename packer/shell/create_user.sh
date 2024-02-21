0f5e90f7d60798b8e85a1ba1f7ebfb57e0d0aa2a
AakashSanganiNeu <sangani.aak@northeastern.edu>
2024-02-21 18:49:02 -0500

    added fmt and removed commented debugs

--- packer/shell/create_user.sh
+++ packer/shell/create_user.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Check if the group exists, and if not, create it
+
 if ! getent group csye6225 >/dev/null; then
   sudo groupadd csye6225
 fi
 
-# Check if the user exists, and if not, create it
 if ! id -u csye6225 >/dev/null 2>&1; then
   sudo useradd -m -g csye6225 csye6225
   echo "User csye6225 created and assigned to group csye6225."
+  sudo useradd -m -g csye6225 csye6225 --shell /usr/sbin/nologin
 else
   echo "User csye6225 already exists."
 fi
