ef990e7e11d07597f4b576b1d9a3dbbc3ac562a7
AakashSanganiNeu <sangani.aak@northeastern.edu>
2024-02-21 19:08:10 -0500

    bug fix

--- packer/shell/mysql_install.sh
+++ packer/shell/mysql_install.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-# Install MySQL
 sudo rpm -Uvh https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
 sudo yum install mysql-server -y
 sudo systemctl start mysqld
 sudo systemctl status mysqld
 
 
-# Enable the new service to start at boot
 sudo systemctl enable mysqld
 
-# Start the new service
 sudo systemctl start mysqld
 
-# Optionally, check the status of the new service
 sudo systemctl status mysqld
 
-# MySQL commands
 mysql -u root <<EOF
 CREATE DATABASE myDatabaseName;
 CREATE USER 'myUsername'@'localhost' IDENTIFIED BY 'myPassword';
--- packer/shell/requirements.sh
+++ packer/shell/requirements.sh
@@ -6,35 +6,8 @@ sudo /tmp/webapp/
 
 unzip -o /tmp/webapp.zip -d /tmp/webapp/
 
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-
-# sudo dnf module list nodejs
-# sudo dnf module enable nodejs:20
-# sudo dnf install -y npm
-
-# #!/bin/bash
-
-# # Install NVM (Node Version Manager)
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
-# # Source NVM scripts to the current session
-# export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-
-# Install Node.js version 21
-# nvm install 21
-
-# # Use Node.js version 21
-# nvm use 21
-
-# # Confirm the installation
-# node -v
-
-# Adding the NodeSource official repository for Node.js 20.x
 curl -fsSL https://rpm.nodesource.com/setup_20.x | sudo bash -
 
-# Installing Node.js and npm
 sudo yum install -y nodejs
 
 cd /tmp/webapp
@@ -53,7 +26,6 @@ HOST=$HOST
 PORT=$PORT
 EOF
 
-#npm install
 npm init -y
 npm install
 npm install express sequelize mysql2 body-parser
