54a37835e5b3d5b32f61a6f200a4d73bcd381e23
AakashSanganiNeu <sangani.aak@northeastern.edu>
2024-03-20 18:24:34 -0400

    adding severity

--- logger.js
+++ logger.js
@@ -1,34 +1,57 @@
 const bunyan = require('bunyan');
 const path = require('path');
+const fs = require('fs');
 
-// Determine the environment and set the log directory accordingly
-const environment = process.env.NODE_ENV || 'development'; 
-let logDirectory = '/var/webapp'; 
-if (environment === 'development') {
-    logDirectory = '/var/webapp';
-} else if (environment === 'test') {
-    logDirectory = '/tmp/webapps';
-} else if (environment === 'production') {
-    logDirectory = '/var/webapp/logs';
+const mapLevelToSeverity = (level) => {
+    return {
+        10: 'trace',
+        20: 'debug',
+        30: 'info',
+        40: 'warn',
+        50: 'error',
+        60: 'fatal',
+    }[level] || 'unknown';
+};
+
+class SeverityStream {
+    constructor(filePath) {
+        this.stream = fs.createWriteStream(filePath, { flags: 'a' });
+    }
+
+    write(record) {
+        if (typeof record === 'string') {
+            record = JSON.parse(record);
+        }
+
+        const clonedRecord = { ...record };
+
+        clonedRecord.severity = mapLevelToSeverity(clonedRecord.level);
+
+        this.stream.write(JSON.stringify(clonedRecord) + '\n');
+    }
 }
 
-// Configure the logger
+const environment = process.env.NODE_ENV || 'development';
+let logDirectory = '/var/webapp';
+switch (environment) {
+    case 'development':
+        logDirectory = '/var/webapp';
+        break;
+    case 'test':
+        logDirectory = '/tmp/webapps';
+        break;
+    case 'production':
+        logDirectory = '/var/webapp/logs';
+        break;
+}
+
 const logger = bunyan.createLogger({
     name: 'myapp',
     streams: [
         {
             level: 'info',
-            type: 'rotating-file',
-            path: path.join(logDirectory, 'myapp.log'),
-            period: '1d', 
-            count: 7 
-        },
-        {
-            level: 'error',
-            type: 'rotating-file',
-            path: path.join(logDirectory, 'myapp.log'),
-            period: '1d', 
-            count: 7 
+            stream: new SeverityStream(path.join(logDirectory, 'myapp.log')),
+            type: 'raw' 
         }
     ]
 });
