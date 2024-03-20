de26af963b2a3d21d8c7e10680fdcc04629750fc
AakashSanganiNeu <sangani.aak@northeastern.edu>
2024-03-19 21:21:31 -0400

    added node env as a variable for logs

--- logger.js
+++ logger.js
@@ -1,13 +1,25 @@
 const bunyan = require('bunyan');
 const path = require('path');
 
+// Determine the environment and set the log directory accordingly
+const environment = process.env.NODE_ENV || 'development'; 
+let logDirectory = '/var/webapp'; 
+if (environment === 'development') {
+    logDirectory = '/var/webapp';
+} else if (environment === 'test') {
+    logDirectory = '/tmp/webapps';
+} else if (environment === 'production') {
+    logDirectory = '/var/webapp/logs';
+}
+
+// Configure the logger
 const logger = bunyan.createLogger({
     name: 'myapp',
     streams: [
         {
             level: 'info',
             type: 'rotating-file',
-            path: path.join('/var/webapp', 'myapp.log'),
+            path: path.join(logDirectory, 'myapp.log'),
             period: '1d', 
             count: 7 
         }
