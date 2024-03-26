f23cdeb10dc88b7c641a5becfce0cd768bf3dc3d
AakashSanganiNeu <sangani.aak@northeastern.edu>
2024-03-26 15:51:37 -0400

    updated tests and incresed timeout

--- test.js
+++ test.js
@@ -6,6 +6,7 @@ const logger = require('./logger');
 
 beforeAll(async () => {
   await sequelize.sync({ force: true });
+  jest.setTimeout(10000);
 });
 
 afterAll(async () => {
@@ -37,7 +38,7 @@ describe('User API Integration Tests', () => {
                             .set('Authorization', authHeader);
     expect(response.status).toBe(200);
     expect(response.body.email).toEqual(userData.email);
-  });
+  }, 10000);
 
   test('Test 2 - Update the account and using the GET call, validate the account was updated', async () => {
     const updateData = {
@@ -50,15 +51,13 @@ describe('User API Integration Tests', () => {
     let response = await request.put('/v1/user/self')
                                 .set('Authorization', authHeader)
                                 .send(updateData);
-    console.log(response.status + "  put failed")
     expect(response.status).toBe(200);
 
     response = await request.get('/v1/user/self')
                             .set('Authorization', authHeader);
-    console.log(response.status + "  puts get failed")
     expect(response.status).toBe(200);
     expect(response.body.firstName).toEqual(updateData.firstName);
     expect(response.body.lastName).toEqual(updateData.lastName);
 
-  });
+  }, 10000);
 });
